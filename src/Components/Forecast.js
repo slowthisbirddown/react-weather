f5398d6bed403d6cf368025bb09c1b13e865b662
Adam Yaccarino <hupo1989@gmail.com>
2021-04-23 16:27:22 -0600

    Started adding function for metric to imperial conversion

--- src/Components/Forecast.js
+++ src/Components/Forecast.js
@@ -9,6 +9,24 @@ import AccordionDetails from '@material-ui/core/AccordionDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+  }
+
+  function toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+  }
+
+  function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+      return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+  }
+
 class Forecast extends React.Component {
     constructor(props){
         super(props);
